ab5a5f6faa300aad3554f59bd0dfeec6ca0415a9
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-10-27 18:59:12 +0000

    Added monogram angle fitness

--- cipher-C++/fitness.cpp
+++ cipher-C++/fitness.cpp
@@ -36,4 +36,20 @@ namespace fitness {
 	double chi2Fitness(std::string text) {
 		return chi2(monogramFrequencies(text, text.length()), corpus::frequencyArray);
 	}
+
+	double dotProduct(std::array<double, 26> vec1, std::array<double, 26> vec2) {
+		double tot = 0.0;
+		for (int i = 0; i < 26; i++) {
+			tot += vec1[i] * vec2[i];
+		}
+		return tot;
+	}
+
+	double angleBetweenVectors(std::array <double, 26 > vec1, std::array<double, 26> vec2) {
+		return dotProduct(vec1, vec2) / sqrt(dotProduct(vec1, vec1) * dotProduct(vec2, vec2));
+	}
+
+	double angleBetweenVectorsFitness(std::string text) {
+		return angleBetweenVectors(monogramFrequencies(text), corpus::frequencyArray);
+	}
 }
--- cipher-C++/fitness.h
+++ cipher-C++/fitness.h
@@ -7,4 +7,7 @@ namespace fitness {
 	std::array<double, 26> monogramFrequencies(std::string text, int divisor);
 	double chi2(std::array<double, 26> expected, std::array<double, 26> actual);
 	double chi2Fitness(std::string text);
+	double dotProduct(std::array<double, 26> vec1, std::array<double, 26> vec2);
+	double angleBetweenVectors(std::array<double, 26> vec1, std::array<double, 26> vec2);
+	double angleBetweenVectorsFitness(std::string text);
 }
