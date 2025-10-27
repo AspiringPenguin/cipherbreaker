554459fc260380f3522ac824ff4af1a4bffce0f9
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-10-27 16:58:51 +0000

    Filled out loading corpus data

--- cipher-C++/corpus.h
+++ cipher-C++/corpus.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "fileLoaders.h"
 
 namespace corpus {
+	//Corpus
+	static auto corpus = fileLoaders::loadFile("D:/cipher-data/corpus.txt");
 
+	//Monogram frequencies 
+	static auto frequencies = fileLoaders::loadMapDouble("D:/cipher-data/lettercountsnospaces.txt");
+
+	//Word data
+	static auto wordFrequencies = fileLoaders::loadMapInt("D:/cipher-data/wordcountsbyfrequency.txt");
+	static auto wordsByFrequency = fileLoaders::loadVector("D:/cipher-data/wordcountsbyfrequency.txt");
+	static auto wordsAlphabetical = fileLoaders::loadVector("D:/cipher-data/wordcountsalphabetical.txt");
+
+	//Tetras
+	static auto tetragramLogarithms = fileLoaders::loadMapDouble("D:/cipher-data/tetras/nospacesalphabeticallogarithms.txt");
 }
