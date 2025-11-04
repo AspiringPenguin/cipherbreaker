598bb4ef39a1b19c7bcd64303ed90aed61bef1b0
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-11-04 17:50:53 +0000

    Added a basic CLI menu system

--- cipher-C++/interface.cpp
+++ cipher-C++/interface.cpp
@@ -19,4 +19,48 @@ namespace cliInterface {
 		}
 		return cipher;
 	}
+
+	//MenuItem
+	MenuItem::MenuItem(std::string s, int(*_func)(std::string))
+	{
+		name = s;
+		func = _func;
+	}
+
+	int MenuItem::run(std::string s)
+	{
+		return func(s);
+	}
+
+	
+	//Menu
+	std::string Menu::getCipher()
+	{ 
+		return cipher;
+	}
+
+	void Menu::setCipher(std::string _cipher)
+	{
+		cipher = _cipher;
+	}
+
+	void Menu::addMenuItem(MenuItem i)
+	{
+		menuItems.push_back(i);
+	}
+
+	int Menu::run()
+	{
+		std::cout << "----------MENU----------" << std::endl;
+		int l = menuItems.size();
+		for (int i = 0; i < l; i++) {
+			std::cout << i + 1 << ". " << menuItems[i].name << std::endl;
+		}
+		std::cout << "> ";
+		std::string inp;
+		getline(std::cin, inp);
+		int pos = std::stoi(inp) - 1;
+		return menuItems[pos].run(cipher);
+	}
+
 }
--- cipher-C++/interface.h
+++ cipher-C++/interface.h
@@ -1,6 +1,25 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace cliInterface {
 	std::string getCipherInput();
+	class MenuItem {
+		int (*func) (std::string);
+	public:
+		std::string name;
+		MenuItem(std::string s, int (*func) (std::string));
+		int run(std::string);
+	};
+
+	class Menu {
+		std::vector<MenuItem> menuItems;
+		std::string cipher;
+
+	public:
+		std::string getCipher();
+		void setCipher(std::string cipher);
+		void addMenuItem(MenuItem i);
+		int run();
+	};
 }
