1fed6c117f772a4633df890d861630b0e757b8c8
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-11-10 17:11:16 +0000

    Added + and - support

--- cipher-C++/modularNumber.cpp
+++ cipher-C++/modularNumber.cpp
@@ -7,15 +7,36 @@ void modularNumber::update() {
 modularNumber::modularNumber(int n) {
 	value = n;
 	modulus = 26;
+	update();
 }
 
 modularNumber::modularNumber(int n, int m) {
 	value = n;
 	modulus = m;
+	update();
 }
 
 modularNumber modularNumber::operator=(int& n) {
 	value = n;
+	update();
+}
+
+modularNumber modularNumber::operator+(int& n) {
+	return modularNumber(value + n);
+}
+
+modularNumber modularNumber::operator+(modularNumber& n) {
+	return modularNumber(value + n.value);
+}
+
+modularNumber modularNumber::operator -(int& n)
+{
+	return modularNumber(value - n);
+}
+
+modularNumber modularNumber::operator -(modularNumber& n)
+{
+	return modularNumber(value - n.value);
 }
 
 std::ostream& operator<<(std::ostream& stream, const modularNumber& mNum) {
--- cipher-C++/modularNumber.h
+++ cipher-C++/modularNumber.h
@@ -12,6 +12,12 @@ public:
 	modularNumber(int n, int m);
 	modularNumber operator=(int& n);
 
+	modularNumber operator+(int& n);
+	modularNumber operator+(modularNumber& n);
+
+	modularNumber operator - (int& n);
+	modularNumber operator - (modularNumber& n);
+
 	//For output, weird, I don't fully get how this works (friend means it can get private values)
 	friend std::ostream& operator<< (std::ostream& stream, const modularNumber& mNum);
 };
