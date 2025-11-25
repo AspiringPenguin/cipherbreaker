20730bcc7dcf6699c75786411100f8b73cb8aef6
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-11-25 10:52:56 +0000

    Added stubs for a vignere cipher implementation

---
+++ cipher-C++/periodic.cpp
@@ -0,0 +1,10 @@
+#include "periodic.h"
+
+std::string vignere(std::string cipher, int keyLen) {
+	return std::string();
+}
+
+std::string vignere(std::string cipher)
+{
+	return std::string();
+}
---
+++ cipher-C++/periodic.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include "strings.h"
+
+std::string vignere(std::string cipher, int keyLen)
+std::string vignere(std::string cipher);
