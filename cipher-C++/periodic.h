dfe99d8121f585808b44bd8382b1238bc7055dc4
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-11-25 10:56:16 +0000

    Fixed an error in periodic.h

--- cipher-C++/periodic.h
+++ cipher-C++/periodic.h
@@ -2,5 +2,5 @@
 #include <string>
 #include "strings.h"
 
-std::string vignere(std::string cipher, int keyLen)
+std::string vignere(std::string cipher, int keyLen);
 std::string vignere(std::string cipher);
