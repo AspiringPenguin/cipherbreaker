39aa20e35e32904d3d3f236d1782cdfa84ccd732
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-05 10:26:13 +0000

    Started work on the solitaire cipher

---
+++ cipher-C++/solitaire.cpp
@@ -0,0 +1,27 @@
+#include "solitaire.h"
+
+namespace solitaire {
+	void applyKeyChar(std::array<int, 54>& deck)
+	{
+	}
+
+	std::array<int, 54> keyDeck(std::string key)
+	{
+		return std::array<int, 54>();
+	}
+
+	int getNext(std::array<int, 54>& deck)
+	{
+		return 0;
+	}
+
+	char combineLetters(int n, char c)
+	{
+		return 0;
+	}
+
+	char decombineLetters(int n, char c)
+	{
+		return 0;
+	}
+}
