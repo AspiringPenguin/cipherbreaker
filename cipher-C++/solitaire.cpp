8c78979486403f83891fda64ec49982e4096cd19
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-15 18:55:46 +0000

    Cleared out solitaire code that I'm not going to use

--- cipher-C++/solitaire.cpp
+++ cipher-C++/solitaire.cpp
@@ -1,27 +1,5 @@
 #include "solitaire.h"
+#include "stream.h" //For combining/decombining characters
 
 namespace solitaire {
-	void applyKeyChar(std::array<int, 54>& deck)
-	{
-	}
-
-	std::array<int, 54> keyDeck(std::string key)
-	{
-		return std::array<int, 54>();
-	}
-
-	int getNext(std::array<int, 54>& deck)
-	{
-		return 0;
-	}
-
-	char combineLetters(int n, char c)
-	{
-		return 0;
-	}
-
-	char decombineLetters(int n, char c)
-	{
-		return 0;
-	}
 }
