918cb06daef955f0c3aa1442b064e56bdc5bdff8
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-16 11:48:40 +0000

    Added code for moving joker A and joker B

--- cipher-C++/main.cpp
+++ cipher-C++/main.cpp
@@ -27,9 +27,15 @@ int testStuff(std::string cipher) {
 	//	std::cout << ms_int.count() << std::endl;
 	//}
 
-	cipher = basics::formatString(cipher);
+	std::array<int, 54> deck = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+								29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54 };
 
-	std::cout << solitaire::decombineCharacters('y', 25) << std::endl;
+	solitaire::moveJokerB(deck);
+
+	for (auto card : deck) {
+		std::cout << card << " ";
+	}
+	std::cout << std::endl;
 
 	return 2;
 }
--- cipher-C++/solitaire.cpp
+++ cipher-C++/solitaire.cpp
@@ -1,4 +1,5 @@
 #include "solitaire.h"
+#include <algorithm>
 
 namespace solitaire {
 	char combineCharacters(char p, int s) { //P between 97 and 122, S between 1 and 26
@@ -15,5 +16,51 @@ namespace solitaire {
 			val += 26;
 		}
 		return val + 96;
+	}
+
+	void moveJokerA(std::array<int, 54>& deck) {
+		auto jokerA = std::distance(deck.begin(), std::find(deck.begin(), deck.end(), 53));
+		if (jokerA == 53) { //Bottom of the deck -> index 1, need to move alot of cards now
+			for (int i = 52; i > 0; i--) {
+				deck[i + 1] = deck[i];
+			}
+			deck[1] = 53;
+		}
+		else { //Just swap with the card below
+			deck[jokerA] = deck[jokerA + 1];
+			deck[jokerA + 1] = 53;
+		}
+	}
+
+	void moveJokerB(std::array<int, 54>& deck) {
+		auto jokerB = std::distance(deck.begin(), std::find(deck.begin(), deck.end(), 54));
+		if (jokerB == 53) { //Bottom of the deck -> index 2, need to move alot of cards now
+			for (int i = 52; i > 1; i--) {
+				deck[i + 1] = deck[i];
+			}
+			deck[2] = 54;
+		}
+		else if (jokerB == 52) { //1 before bottom of the deck -> index 1, need to move alot of cards now
+			for (int i = 51; i > 0; i--) {
+				deck[i + 1] = deck[i];
+			}
+			deck[1] = 54;
+		}
+		else { //Move down by two cards
+			deck[jokerB] = deck[jokerB + 1];
+			deck[jokerB + 1] = deck[jokerB + 2];
+			deck[jokerB + 2] = 54;
+		}
+
+	}
+
+	void jokerTripleCut(std::array<int, 54>& deck) {
+	}
+
+	void bottomCountCut(std::array<int, 54>& deck) {
+	}
+
+	int generateKeyStreamNum(std::array<int, 54>& deck) {
+		 return 0;
 	}
 }
