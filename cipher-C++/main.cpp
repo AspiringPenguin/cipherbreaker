34a345ad91b6f7674bd33836338a799b72077dd8
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-17 12:17:02 +0000

    Updated some menu labels

--- cipher-C++/main.cpp
+++ cipher-C++/main.cpp
@@ -52,8 +52,8 @@ int main() {
 	menu.addMenuItem(cliInterface::MenuItem("Vigenere (Multiple Caesar Shifts)", *periodic::cliVigenere));
 	menu.addMenuItem(cliInterface::MenuItem("Polyalphabetic Substitution (Hill Climber)", *periodic::cliHillClimber));
 
-	menu.addMenuItem(cliInterface::MenuItem("Permutation (Brute Force)", *transpositions::cliPermutationBruteForce));
-	menu.addMenuItem(cliInterface::MenuItem("Columnar (Brute Force)", *transpositions::cliColumnarBruteForce));
+	menu.addMenuItem(cliInterface::MenuItem("Permutation Transposition (Brute Force)", *transpositions::cliPermutationBruteForce));
+	menu.addMenuItem(cliInterface::MenuItem("Columnar Transposition (Brute Force)", *transpositions::cliColumnarBruteForce));
 
 	menu.addMenuItem(cliInterface::MenuItem("Playfair (Wandering Hill Climber)", *polybius::cliPlayfairHillClimber));
 	menu.addMenuItem(cliInterface::MenuItem("Playfair 2025 Variation (Wandering Hill Climber)", *polybius::cliPlayfair2025VariationHillClimber));
