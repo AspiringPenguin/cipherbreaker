4ea537db28a64de2259ae1ff9f0f68bc0f3c8d57
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-22 08:14:55 +0000

    Standardised comment locations

--- cipher-C++/corpus.cpp
+++ cipher-C++/corpus.cpp
@@ -1,7 +1,8 @@
 #include "corpus.h"
 
 namespace corpus {
-	std::array<float, 26> frequenciesAsArray(std::unordered_map<std::string, float> map) { //Convert a hashmap to an array, used for single characters so a=index 0 etc.
+	//Convert a hashmap to an array, used for single characters so a=index 0 etc.
+	std::array<float, 26> frequenciesAsArray(std::unordered_map<std::string, float> map) { 
 		std::array<float, 26> array;
 		char c;
 		for (int i = 0; i < 26; i++) {
@@ -11,10 +12,9 @@ namespace corpus {
 		return array;
 	}
 
+	//Specialised case to convert tetragram frequencies/logs to an array
+	//This puts each tetragram at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
 	std::vector<float> tetragramsAsArray(std::unordered_map<std::string, float> map) {
-		//Specialised case to convert tetragram frequencies/logs to an array
-		//This puts each tetragram at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
-
 		auto arrays = std::vector<float>();
 		for (char i = 0; i < 26; i++) {
 			for (char j = 0; j < 26; j++) {
@@ -28,11 +28,10 @@ namespace corpus {
 		return arrays;
 	}
 
+	//This was added to reduce the number of random memory accesses in tetragram fitness
+	//By combing the logarithms of each possible pair of two tetragrams into a quintgram, which leads to roughly halving random memory accesses in tetragram fitness
+	//The qintgrams are still put at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
 	std::vector<float> precombineTetragrams(std::vector<float>& tetragrams) {
-		//This was added to reduce the number of random memory accesses in tetragram fitness
-		//By combing the logarithms of each possible pair of two tetragrams into a quintgram, which leads to roughly halving random memory accesses in tetragram fitness
-		//The qintgrams are still put at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
-
 		auto vec = std::vector<float>();
 		
 		float subtot;
