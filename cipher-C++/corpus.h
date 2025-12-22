d04e2fd07cd115e2b4b598911cbe86acc2e05276
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-22 08:07:49 +0000

    Refactored corpus.h to have a fixed location for cipher data and just add relative paths for each loader

--- cipher-C++/corpus.h
+++ cipher-C++/corpus.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "fileLoaders.h"
 #include <array>
+#include <string>
 
 namespace corpus {
+	static std::string dataLoc = "D:/cipher-data/";
+
 	//Corpus
-	static auto corpus = fileLoaders::loadFile("D:/cipher-data/corpus.txt");
+	static auto corpus = fileLoaders::loadFile(dataLoc + "corpus.txt");
 
 	//Monogram frequencies 
-	static auto frequencies = fileLoaders::loadMapDouble("D:/cipher-data/lettercountsnospaces.txt");
+	static auto frequencies = fileLoaders::loadMapDouble(dataLoc + "lettercountsnospaces.txt");
 
 	std::array<float, 26> frequenciesAsArray(std::unordered_map<std::string, float> map);
 	std::vector<float> tetragramsAsArray(std::unordered_map<std::string, float> map);
@@ -16,12 +19,12 @@ namespace corpus {
 	static auto frequencyArray = frequenciesAsArray(frequencies);
 
 	//Word data
-	static auto wordFrequencies = fileLoaders::loadMapInt("D:/cipher-data/wordcountsbyfrequency.txt");
-	static auto wordsByFrequency = fileLoaders::loadVector("D:/cipher-data/wordcountsbyfrequency.txt");
-	static auto wordsAlphabetical = fileLoaders::loadVector("D:/cipher-data/wordcountsalphabetical.txt");
+	static auto wordFrequencies = fileLoaders::loadMapInt(dataLoc + "wordcountsbyfrequency.txt");
+	static auto wordsByFrequency = fileLoaders::loadVector(dataLoc + "wordcountsbyfrequency.txt");
+	static auto wordsAlphabetical = fileLoaders::loadVector(dataLoc + "wordcountsalphabetical.txt");
 
 	//Tetras
-	static auto tetragramLogarithms = fileLoaders::loadMapDouble("D:/cipher-data/tetras/nospacesalphabeticallogarithms.txt");
+	static auto tetragramLogarithms = fileLoaders::loadMapDouble(dataLoc + "tetras/nospacesalphabeticallogarithms.txt");
 	static auto tetragramArrays = tetragramsAsArray(tetragramLogarithms);
 
 	static auto combinedTetragrams = precombineTetragrams(tetragramArrays);
