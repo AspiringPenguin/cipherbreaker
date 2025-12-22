62f7a77114afe00cc2092efac4ed38aba7dc0d52
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-22 08:07:54 +0000

    Commented corpus.cpp

--- cipher-C++/corpus.cpp
+++ cipher-C++/corpus.cpp
@@ -1,7 +1,7 @@
 #include "corpus.h"
 
 namespace corpus {
-	std::array<float, 26> frequenciesAsArray(std::unordered_map<std::string, float> map) {
+	std::array<float, 26> frequenciesAsArray(std::unordered_map<std::string, float> map) { //Convert a hashmap to an array, used for single characters so a=index 0 etc.
 		std::array<float, 26> array;
 		char c;
 		for (int i = 0; i < 26; i++) {
@@ -12,6 +12,9 @@ namespace corpus {
 	}
 
 	std::vector<float> tetragramsAsArray(std::unordered_map<std::string, float> map) {
+		//Specialised case to convert tetragram frequencies/logs to an array
+		//This puts each tetragram at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
+
 		auto arrays = std::vector<float>();
 		for (char i = 0; i < 26; i++) {
 			for (char j = 0; j < 26; j++) {
@@ -26,6 +29,10 @@ namespace corpus {
 	}
 
 	std::vector<float> precombineTetragrams(std::vector<float>& tetragrams) {
+		//This was added to reduce the number of random memory accesses in tetragram fitness
+		//By combing the logarithms of each possible pair of two tetragrams into a quintgram, which leads to roughly halving random memory accesses in tetragram fitness
+		//The qintgrams are still put at the base 26 index specified by its letters e.g. aaaa=0, aaab=1 etc
+
 		auto vec = std::vector<float>();
 		
 		float subtot;
