64aa1a53d0c2b28dc1a732d3111940d915e3c35a
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-23 16:25:03 +0000

    Commented periodic.cpp

--- cipher-C++/periodic.cpp
+++ cipher-C++/periodic.cpp
@@ -61,20 +61,24 @@ namespace periodic {
 		return 0; //Failure
 	}
 
+	//Polyalphabetic decrypt, with a list of monoalphabetic keys, one for each 'column' and a bool for if it is an encryption key which just gets passed through
 	std::string polyalphabeticDecrypt(std::string cipher, std::vector<std::array<char, 26>> key, bool encryptionKey) {
-		int n = key.size();
+		int n = key.size(); 
 		auto cols = strings::getColumns(cipher, n);
-		auto newCols = std::vector<std::string>();
+		auto newCols = std::vector<std::string>(); //To put decrypted columns in
 		for (int i = 0; i < n; i++){
-			newCols.push_back(monoalphabetic::decrypt(cols[i], key[i], encryptionKey));
+			newCols.push_back(monoalphabetic::decrypt(cols[i], key[i], encryptionKey)); //Decrypt each 'column' with the relevant key
 		}
-		return strings::columnsToString(newCols);
+		return strings::columnsToString(newCols); //COmbine the decrypted columns into a text
 	}
 
+	//Hill climbing attack on periodic substitution ciphers
+	//As described in Madness's book at the end of the periodic ciphers section, with just a different limit calculation
+	//Uses a custom hill climbing attack, see "periodic substitution hill climber"
 	std::string hillClimber(std::string cipher, int keySize) {
 		int l = cipher.length();
 
-		//Generate best key
+		//Generate best key as a list of copies of the alphabet
 		auto alphabetKey = monoalphabetic::stringToKey(basics::alphabet);
 		auto bestKey = std::vector<std::array<char, 26>>();
 		for (int i = 0; i < keySize; i++) {
@@ -88,7 +92,8 @@ namespace periodic {
 
 		float bestFitness = fitness::tetragramFitness(&bestDecrypt);
 
-		int limit = (keySize * keySize) * 100000000 / l;
+		int limit = (keySize * keySize) * 100000000 / l; // A variation on something I found in an academic article to make long texts finish more quickly
+														 // without compromising accuracy on short texts
 
 		//Set up for vars in loops
 		int littleCounter;
@@ -106,7 +111,7 @@ namespace periodic {
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dist(0, 25);
 
-		std::ios_base::sync_with_stdio(false);
+		std::ios_base::sync_with_stdio(false); //Makes std::cout faster so it isn't a bottleneck to print status updates
 
 		std::cout << limit << std::endl;
 
@@ -114,12 +119,18 @@ namespace periodic {
 		while (bigCounter < limit) {
 			std::cout << bigCounter << std::endl;
 			for (int i = 0; i < keySize; i++) {
+				//Set parent key to a copy of the best key and shuffle the ith subkey
 				parentKey = bestKey;
 				std::shuffle(std::begin(parentKey[i]), std::end(parentKey[i]), gen);
+
+				//Decrypt and get fitness with parent key
 				parentDecrypt = polyalphabeticDecrypt(cipher, parentKey, false);
 				parentFitness = fitness::tetragramFitness(&parentDecrypt);
+
+				//Reset littleCounter
 				littleCounter = 0;
 				while (littleCounter < 1000) {
+					//Try swapping two letters in the ith subkey
 					childKey = parentKey;
 					x = dist(gen);
 					y = dist(gen);
@@ -129,18 +140,23 @@ namespace periodic {
 					_ = childKey[i][x];
 					#pragma warning( push )
 					#pragma warning( disable : 28020 )
-					childKey[i][x] = childKey[i][y]; //warning: ignore
+					childKey[i][x] = childKey[i][y]; 
 					#pragma warning( pop )
 					childKey[i][y] = _;
+
+					//Decrypt with childKey and get fitness
 					childDecrypt = polyalphabeticDecrypt(cipher, childKey, false);
 					childFitness = fitness::tetragramFitness(&childDecrypt);
+
+					//Inner loop and parentKey checks
 					if (childFitness > parentFitness) {
 						parentKey = childKey;
 						parentDecrypt = childDecrypt;
 						parentFitness = childFitness;
 						littleCounter = 0;
 					}
-					littleCounter++;
+
+					//Outer loop and bestKey checks
 					if (childFitness > bestFitness) {
 						std::cout << childFitness << std::endl;
 						bestDecrypt = childDecrypt;
@@ -153,13 +169,17 @@ namespace periodic {
 							}
 						}
 					}
+					
+					//Increment counters
+					littleCounter++;
 					bigCounter++;
 				}
 			}
 		}
 
-		std::ios_base::sync_with_stdio(true);
+		std::ios_base::sync_with_stdio(true); //Resyncs std::cout to prevent issues elsewhere
 
+		//Print best key
 		for (int n = 0; n < keySize; n++) {
 			std::cout << monoalphabetic::keyToString(monoalphabetic::invertKey(bestKey[n])) << std::endl;
 		}
@@ -167,6 +187,7 @@ namespace periodic {
 		return polyalphabeticDecrypt(cipher, bestKey, false);
 	}
 
+	//Overload for the above that determines keylength then calls the above
 	std::string hillClimber(std::string cipher) {
 		//Get key size
 		int keySize = getKeySize(cipher);
@@ -177,12 +198,16 @@ namespace periodic {
 		return hillClimber(cipher, keySize);
 	}
 
+	//CLI Interface code for the above
 	int cliHillClimber(std::string cipher) {
 		cipher = basics::formatString(cipher);
+
 		std::string decrypt = hillClimber(cipher);
-		if (decrypt == "") {
+
+		if (decrypt == "") { //Only occurs if key length couldn't be determined
 			return 0; //Failure
 		}
+
 		if (fitness::tetragramFitness(&decrypt) > -15) {
 			if (cliInterface::offerDecryption(decrypt)) {
 				return 1; //Success
