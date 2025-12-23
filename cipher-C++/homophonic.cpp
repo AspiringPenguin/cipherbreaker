85d4a846a7d1f0cc209d84a8801c614865f1043a
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-23 13:50:50 +0000

    Finished commenting homophonic.cpp and added a reference for different hill-climber variation

--- cipher-C++/homophonic.cpp
+++ cipher-C++/homophonic.cpp
@@ -6,6 +6,7 @@
 #include <random>
 
 namespace homophonic {
+	//A very simple decryption routine which takes a map of which plaintext character each ciphertext character is defined as 
 	std::string homophonicDecrypt(std::string cipher, std::unordered_map<char, char> key) {
 		std::string plain = "";
 		for (char c : cipher) {
@@ -14,10 +15,12 @@ namespace homophonic {
 		return plain;
 	}
 
+	// A modified hill-climbing attack on the homophonic substitution cipher
+	// This uses the "basic best-current-child key" setup, as explained in hillClimberTypes.txt
 	std::unordered_map<char, char> homophonicHillClimber(std::string cipher) {
 		cipher = basics::removeSpaces(cipher);
 
-		//Random
+		//Set up stuff for random numbers
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dist(0, 25);
@@ -25,6 +28,7 @@ namespace homophonic {
 		std::uniform_int_distribution<> die(1, 20);
 
 		//Keys
+		//Generate random bestKey, a list of symbols in the ciphertext and the number of symbols in the ciphertext
 		auto bestKey = std::unordered_map<char, char>();
 		auto symbols = std::vector<char>();
 		int nSymbols = 0;
@@ -38,8 +42,10 @@ namespace homophonic {
 			bestKey[c] = dist(gen) + 97; 
 		}
 
+		//Another random number range to select random symbols
 		std::uniform_int_distribution<> symbol(0, nSymbols-1);
 
+		//Set up other key variables
 		std::unordered_map<char, char> currentKey = bestKey;
 		std::unordered_map<char, char> childKey;
 
@@ -52,10 +58,12 @@ namespace homophonic {
 		float currentFitness = bestFitness;
 		float childFitness;
 
+		//Variables to control the attack
 		int counter = 0;
 		int impatience = 0;
 		bool wandering = false;
 
+		//Variables for temp storage when modifying keys
 		int a, b;
 		char _;
 		char ac, bc;
@@ -63,7 +71,8 @@ namespace homophonic {
 		while (counter < 1000000) {
 			childKey = currentKey;
 
-			if (coin(gen) == 0) { //Swap two symbols
+			if (coin(gen) == 0) { //Flip a coin
+				//Swap two ciphertext symbols 
 				a = symbols[symbol(gen)];
 				b = symbols[symbol(gen)];
 				while (a == b) {
@@ -73,7 +82,8 @@ namespace homophonic {
 				childKey[a] = childKey.at(b);
 				childKey[b] = _;
 			}
-			else { //Swap two letters
+			else {
+				//Swap two plaintext letters
 				a = dist(gen);
 				b = dist(gen);
 				while (a == b) {
@@ -91,6 +101,7 @@ namespace homophonic {
 				}
 			}
 
+			//Re-decrypt the text with the child key and get the new fitness
 			childDecrypt = homophonicDecrypt(cipher, childKey);
 			childFitness = fitness::tetragramFitness(&childDecrypt) * (2 - fitness::angleBetweenVectorsFitness(childDecrypt));
 
@@ -144,29 +155,33 @@ namespace homophonic {
 		return bestKey;
 	}
 
+	//A decryptor for a more constrained homophonic substitution cipher, with different key-alphabets for uppercase and lowercase ciphertext characters
 	std::string dualBetDecrypt(std::string cipher, std::array<char, 26> upper, std::array<char, 26> lower) {
 		std::string plain = "";
 		for (char c : cipher) {
 			if (std::isupper(c)) {
-				plain += upper[c - 65];
+				plain += upper[c - 65]; //A = 65 in ASCII & Unicode
 			}
 			else {
-				plain += lower[c - 97];
+				plain += lower[c - 97]; //a = 97 in ASCII & Unicode
 			}
 		}
 		return plain;
 	}
 
+	// A modified version of my hill-climbing attack on the homophonic substitution cipher
+	// This uses the "basic best-current-child key" setup, as explained in hillClimberTypes.txt
 	std::tuple<std::array<char, 26>, std::array<char, 26>> dualBetHillClimber(std::string cipher) {
 		cipher = basics::removeSpaces(cipher);
 
-		//Random
+		//Set up stuff for random numbers
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dist(0, 25);
 		std::uniform_int_distribution<> coin(0, 1);
 		std::uniform_int_distribution<> die(1, 20);
 		
+		//keys
 		std::array<char, 26> bestUpperKey = monoalphabetic::stringToKey("abcdefghijklmnopqrstuvwxyz");
 		std::array<char, 26> bestLowerKey = monoalphabetic::stringToKey("zyxwvutsrqponmlkjihgfedcba");
 		std::array<char, 26> currentUpperKey = bestUpperKey;
@@ -174,6 +189,7 @@ namespace homophonic {
 		std::array<char, 26> childUpperKey;
 		std::array<char, 26> childLowerKey;
 
+		//Decrypts and fitness
 		std::string bestDecrypt = dualBetDecrypt(cipher, bestUpperKey, bestLowerKey);
 		std::string currentDecrypt = bestDecrypt;
 		std::string childDecrypt;
@@ -182,30 +198,36 @@ namespace homophonic {
 		float currentFitness = bestFitness;
 		float childFitness;
 
+		//Vars to control attack
 		int counter = 0;
 		int impatience = 0;
 		bool wandering = false;
 
+		//Temp storage for key modifications
 		char a, b, _;
 
 		while (counter < 1000000) {
 			childUpperKey = currentUpperKey;
 			childLowerKey = currentLowerKey;
 
+			//Get places to swap in a key
 			a = dist(gen);
 			b = dist(gen);
 
-			if (coin(gen) == 0) {
+			if (coin(gen) == 0) { //Flip a coin
+				//Swap in uppercase key
 				_ = childUpperKey[a];
 				childUpperKey[a] = childUpperKey[b];
 				childUpperKey[b] = _;
 			}
 			else {
+				//Swap in lowercase key
 				_ = childLowerKey[a];
 				childLowerKey[a] = childLowerKey[b];
 				childLowerKey[b] = _;
 			}
 
+			//Decrypt and get fitness with child key
 			childDecrypt = dualBetDecrypt(cipher, childUpperKey, childLowerKey);
 			childFitness = fitness::tetragramFitness(&childDecrypt);
 
