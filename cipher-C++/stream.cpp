79f998b627d9246381d9103d13f5a2a0424ed444
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-29 10:59:28 +0000

    Commented stream cipher code

--- cipher-C++/stream.cpp
+++ cipher-C++/stream.cpp
@@ -79,11 +79,12 @@ namespace stream {
 		return 97 + val;
 	}
 
+	//Decryption routine for the autokey cipher for a given key and mode of operation
 	std::string autokeyDecrypt(std::string cipher, std::string key, mode m) {
 		std::string plain = "";
-		plain.reserve(cipher.length());
-		std::queue<char> stream;
-		for (char c : key) {
+		plain.reserve(cipher.length()); //Make the plain big enough to store the decrypt with allocating more memory on the fly
+		std::queue<char> stream; //A queue for the stream so characters can easily be added at the back and removed at the from without any complex operations
+		for (char c : key) { //Put the key into the stream queue
 			stream.push(c);
 		}
 
@@ -92,41 +93,49 @@ namespace stream {
 		for (char c : cipher) {
 			pChar = decombineChars(c, stream.front(), m);
 			stream.pop(); //Have to read then remove from the queue
-			stream.push(pChar);
+			stream.push(pChar); //Add the plaintext char to the queue
 			plain += pChar;
 		}
 
 		return plain;
 	}
 
+	//Mini hillclimber for autokey cipher for a given keyLength and mode of operation
+	//Uses the "check all key changes" setup
 	std::string autokeyMiniHillClimber(std::string cipher, int l, mode m) {
 		cipher = basics::formatString(cipher);
 
-		std::string bestKey = "";
 
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dist(0, 25);
 
+		//Generate a random bestKey
+		std::string bestKey = "";
 		for (int i = 0; i < l; i++) {
-			bestKey += basics::alphabet[dist(gen)];
+			bestKey += (dist(gen) + 97);
 		}
 
+		//Decrypt and get fitness using the bestKey
 		std::string bestDecrypt = autokeyDecrypt(cipher, bestKey, m);
 		float bestFitness = fitness::tetragramFitness(&bestDecrypt);
 
+		//Child values
 		std::string childKey = "";
 		std::string childDecrypt = "";
 		float childFitness = 0.0f;
 
+		//Loop control
 		bool flag = false;
 		
 		while (!flag) {
 			flag = true;
-			for (int i = 0; i < l; i++) {
-				childKey = bestKey;
-				for (char x = 97; x < 123; x++) {
-					childKey[i] = x;
+			for (int i = 0; i < l; i++) { //For pos in key
+				childKey = bestKey; //Copy bestKey
+				for (char x = 97; x < 123; x++) { //For char a=97 -> z=122
+					childKey[i] = x; //Change key
+
+					//Redecrypt and get fitness
 					childDecrypt = autokeyDecrypt(cipher, childKey, m);
 					childFitness = fitness::tetragramFitness(&childDecrypt);
 
@@ -142,26 +151,27 @@ namespace stream {
 		return bestKey;
 	}
 
+	//The outer brute force element for the above, trying every mode and key length
 	std::tuple<std::string, mode> autokeyHillClimber(std::string cipher) {
 		cipher = basics::formatString(cipher);
 		std::string result = "";
-		std::string decrypt =  "";
-		for (int l = 1; l < 21; l++) {
-			for (mode m : {add, subtractKey, subtractText}) {
-				result = autokeyMiniHillClimber(cipher, l, m);
+		std::string decrypt = "";
+		for (int l = 1; l < 21; l++) { //For key length 1->20 inclusive
+			for (mode m : {add, subtractKey, subtractText}) { //For each possible mode
+				result = autokeyMiniHillClimber(cipher, l, m); //Run the hillclimber
 				decrypt = autokeyDecrypt(cipher, result, m);
 				if (fitness::tetragramFitness(&decrypt) > -15) {
 					return { result, m };
 				}
 			}
 		}
-		return { "", add };
+		return { "", add }; //Null result
 	}
 
 	int cliAutokeyHillClimber(std::string cipher) {
 		cipher = basics::formatString(cipher);
 		auto res = autokeyHillClimber(cipher);
-		if (std::get<0>(res) == "") {
+		if (std::get<0>(res) == "") { //Skip null results
 			return 0; //Failure
 		}
 		std::string decrypt = autokeyDecrypt(cipher, std::get<0>(res), std::get<1>(res));
@@ -173,35 +183,41 @@ namespace stream {
 		return 0; //Failure
 	}
 
+	//Convert a progressive vigenere cipher to a vigenere cipher for a given length and progression
 	std::string progressiveVigenereAsVigenere(std::string cipher, int l, int prog) {
 		std::string vigenere = "";
 		for (int i = 0; i < cipher.length(); i++) {
-			vigenere += decombineChars(cipher[i], (((i/l)*prog)%26)+97, add);
+			vigenere += decombineChars(cipher[i], (((i/l)*prog)%26)+97, add); // (i/l) is integer division, so always rounds down. 
+			//Multiply by progression and take mod 26 before adding the ascii offset = 97 (a=97) to generate the char to 'decombine' from the text using addition mode
 		}
 		return vigenere;
 	}
 
+	//A routine to break a progressive vigenere cipher using the above code and the existing attack for a given progression index and progression length
 	std::string progressiveVigenereSubBruteForce(std::string cipher, int l, int prog) {
 		return periodic::vigenereAsCaesarShifts(progressiveVigenereAsVigenere(cipher, l, prog), l);
 	}
 
+	//A brute force attack trying every combination for the above
 	std::string progressiveVigenereBruteForce(std::string cipher) {
 		cipher = basics::formatString(cipher);
-		for (int l = 2; l < 21; l++) {
-			for (int prog = 1;prog < 26;prog++) {
-				std::string text = progressiveVigenereSubBruteForce(cipher, l, prog);
+		for (int l = 2; l < 21; l++) { //For all progression lengths 2->20 inclusive
+			for (int prog = 1; prog < 26; prog++) { //For all progression indexes 1->25
+				std::string text = progressiveVigenereSubBruteForce(cipher, l, prog); //Try the length and indexes
 				if (fitness::tetragramFitness(&text) > -15) {
 					return text;
 				}
 			}
 		}
-		return "";
+		return ""; //Return a null value
 	}
 
 	int cliProgressiveVigenereBruteForce(std::string cipher) {
 		std::string decrypt = progressiveVigenereBruteForce(cipher);
-		if (cliInterface::offerDecryption(decrypt)) {
-			return 1; //Success
+		if (decrypt != "") { //Skip null results
+			if (cliInterface::offerDecryption(decrypt)) {
+				return 1; //Success
+			}
 		}
 		return 0; //Failure
 	}
