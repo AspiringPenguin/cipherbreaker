60d41dd6e0f9bd86105ebd3e4ef3978e75810ebc
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-07-31 11:35:15 +0100

    Moved the build script to a better location

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 echo "Clearing out old files"
+cd cipher-C++
 rm -rf build
-cd generated
+cd ../generated
 rm cipher-C++.exe
-cd ../
+cd ../cipher-C++
 mkdir build
 cd build
 echo "Compiling"
@@ -10,6 +11,6 @@ g++ -c -O3 -std=c++23 ../*.cpp
 echo "Linking"
 g++ -o cipher-C++.exe *.o
 chmod +x cipher-C++.exe
-mv cipher-C++.exe ../generated
+mv cipher-C++.exe ../../generated
 cd ../
 echo "Done"
